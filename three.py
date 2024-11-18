7b54bd151fe4757ffbebf22f23e0c9c04a5ef6cd
VijayKumar MS <nklvijay@gmail.com>
2024-11-18 07:29:15 +0530

    Add files via upload

---
+++ three.py
@@ -0,0 +1,8 @@
+#---type
+name=input('Enter the name :')
+mark1=int(input('Enter the Mark1 :'))
+mark2=float(input('Enter the Mark2 :'))
+
+print("Type of name ",type(name))
+print("Type of Mark1 ",type(mark1))
+print("Type of Mark1 ",type(mark2))
